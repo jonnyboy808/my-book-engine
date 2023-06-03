814c88544d542bbe4a488acb6e652da45aa2da59
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-06-02 19:39:27 -0700

    added queryme to savedbooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -10,9 +10,14 @@ import {
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useMutation, useQuery } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
+  const { loading, data } = useQuery(QUERY_ME);
   const [userData, setUserData] = useState({});
+  const [deleteBook, { error }] = useMutation(REMOVE_BOOK);
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
